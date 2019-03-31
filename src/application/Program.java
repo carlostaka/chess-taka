5ba93b934721be7c36fc5dc1f79369e6f10fa50c
Carlos Taka <carlostaka@gmail.com>
2019-03-31 17:33:24 -0300

    Inicio da implementação

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@
  */
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 /**
  *
@@ -13,7 +13,6 @@ import boardgame.Position;
  */
 public class Program {
     public static void main (String [] args){
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8,8);
     }
 }
