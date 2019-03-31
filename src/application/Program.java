47d3d18458eb2c20f2198524b822fa9d830e835e
Carlos Taka <carlostaka@gmail.com>
2019-03-31 17:19:56 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import boardgame.Position;
+
+/**
+ *
+ * @author carlos
+ */
+public class Program {
+    public static void main (String [] args){
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
